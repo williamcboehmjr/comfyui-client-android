c4aae0f8db403a63c85f68565413afba949b0308
Chuck <chuck@Chucks-MacBook-Air.local>
2026-06-01 16:56:06 -0700

    chore: bump version to 1.0.8 and package release APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.comfyprompt"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
     }
 
     buildTypes {
